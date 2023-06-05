07e4df725681172da41c979f3652a431d3757f5f
Gabriela Cruz <gcruzti@gmail.com>
2023-06-05 20:08:18 -0300

    add: user services

---
+++ src/services/user-service.js
@@ -0,0 +1,23 @@
+import apiFetch from "./api-fetch.js";
+import { tokenKey } from "./config.js";
+
+export async function createUser (
+    newUser = { email, password, first_name, last_name, phone }) {
+    const { token, ...user} = await apiFetch("signup", { body: newUser });
+    sessionStorage.setItem(tokenKey, token);
+    return user;
+}
+
+export async function updateUser (
+    newData = { email, password, first_name, last_name, phone }) {
+    const { token, ...user} = await apiFetch("profile", { 
+        method: "PATCH",
+        body: newData });
+    
+    return user;
+}
+
+export async function getUser () {
+    const { token, ...user} = await apiFetch("profile");
+    return user;
+}
