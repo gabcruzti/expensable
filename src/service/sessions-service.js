75f6a005a96c1b3202d5c96317e30b533c5af55e
Gabriela Cruz <gcruzti@gmail.com>
2023-06-05 08:25:56 -0300

    add: apifetch method

--- src/service/sessions-service.js
+++ src/service/sessions-service.js
@@ -1,51 +1,11 @@
-const BASE_URI = "https://expensable-api.herokuapp.com/";
-const tokenKey = "expensable_token"
+import apiFetch from "./api-fetch.js";
 
 export async function login (credentials = { email, password }) {
-    const response = await fetch(`${BASE_URI}/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(credentials),
-    });
-
-    if(!response.ok) {
-        const data = await response.json();
-        throw new Error(data.errors);
-    }
-
-    const data = await response.json();
-    sessionStorage.setItem(tokenKey, data.token);
-
-    return data;
+    return apiFetch("login", { body: credentials })
 }
 
 export async function logout () {
-    const token = sessionStorage.getItem(tokenKey);
-
-    const response = await fetch(`${BASE_URI}/logout`, {
-        method: "DELETE",
-        headers: {
-            Authorization: `Token token=${token}`,
-        },
-    });
-
-    let data;
-
-    try {
-        
-        data = await response.json();
-    } catch (error) {
-        data = response.statusText;
-    }
-
-    if(!response.ok) {
-        throw new Error(data.errors);
-    }
-
-    sessionStorage.removeItem(tokenKey);
-    return data;
+    return apiFetch("logout", { method: "DELETE"})
 
 }
 
